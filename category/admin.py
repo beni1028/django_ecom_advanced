5a069777f6f8d133f43fd9e433cd93075a4f1494
beni1028 <beni1028@gmail.com>
2022-07-06 19:33:28 +0530

    Custom-User Model

--- category/admin.py
+++ category/admin.py
@@ -9,4 +9,4 @@ from .models import Category
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
 	list_display = ['category_name', 'slug']
-	prepopulated_fields = {'slug': ('category_name','id')}
+	prepopulated_fields = {'slug': ('category_name',)}
--- ecom/settings.py
+++ ecom/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'store',
     'category',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -137,3 +138,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'accounts.Account'
